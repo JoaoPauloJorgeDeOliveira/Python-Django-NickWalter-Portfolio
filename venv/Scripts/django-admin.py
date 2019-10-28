de6cfe416f62b0b1fc682eaec93ac632c79d1a7d
João Paulo Jorge de Oliveira <joaoolive@gmail.com>
2019-10-28 13:44:18 -0300

    Added jobs and blog apps

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joão paulo\google drive (joaoolive@gmail.com)\programação\django 2.2 - udemy - nick walter\portfolio\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
