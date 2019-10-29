155d736fdd2ca4a8891c6df6722fe46430525024
João Paulo Jorge de Oliveira <joaoolive@gmail.com>
2019-10-29 23:25:42 +0000

    Recreated venv. Site is online.

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\joão paulo\google drive (joaoolive@gmail.com)\programação\django 2.2 - udemy - nick walter\portfolio\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
